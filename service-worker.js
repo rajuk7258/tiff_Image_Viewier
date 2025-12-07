fb85e010a917e77c1a2fc46fcc82a65fe31afe3f
rajuk7258 <127686821+rajuk7258@users.noreply.github.com>
2025-12-07 14:45:12 +0530

    Refactor service worker event listeners for clarity

--- service-worker.js
+++ service-worker.js
@@ -9,11 +9,11 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
